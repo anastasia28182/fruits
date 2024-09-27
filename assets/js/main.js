bb6e65b0630d61fcdbf72438a8100c010f3c3ade
Анастасия Король <gr433_koadm@lab-402-9.tomtit-tomsk.ru>
2024-09-27 15:59:26 +0700

    init

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+let name : string = '';
+let game  = {};
+let panel= 'start';
+let $ = function (domElement) { return document.querySelector(domElement); };
+
+let nav = () => {
+    document.onclick = (event :) => {
+        event.preventDefault();
+        switch ( event.target.id) {
+            case "start"
+        }
+    }
+}
